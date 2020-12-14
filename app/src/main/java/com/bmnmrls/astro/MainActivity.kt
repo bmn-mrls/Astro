b8106a0d29375ca6ed380b831ccf8f3d2c39c6e0
Benjamin Morales <bmnmrls@gmail.com>
2020-12-13 20:51:19 -0600

    🎉 Initial commit

---
+++ app/src/main/java/com/bmnmrls/astro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bmnmrls.astro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
