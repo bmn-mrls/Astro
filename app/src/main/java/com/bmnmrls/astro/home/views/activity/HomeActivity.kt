6e7f55dee76b08ab737f0ff6d5c4e9d6013bc079
Benjamin Morales <bmnmrls@gmail.com>
2021-01-02 00:00:46 -0600

    ✨ Add base navigation structure

--- app/src/main/java/com/bmnmrls/astro/home/views/activity/HomeActivity.kt
+++ app/src/main/java/com/bmnmrls/astro/home/views/activity/HomeActivity.kt
@@ -4,13 +4,31 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.bmnmrls.astro.R
+import com.bmnmrls.astro.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            navController
+        )
     }
 
     companion object {
--- app/src/main/java/com/bmnmrls/astro/home/views/fragments/SavedFragment.kt
+++ app/src/main/java/com/bmnmrls/astro/home/views/fragments/SavedFragment.kt
@@ -3,6 +3,6 @@ package com.bmnmrls.astro.home.views.fragments
 import androidx.fragment.app.Fragment
 import com.bmnmrls.astro.R
 
-class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
+class SavedFragment : Fragment(R.layout.fragment_saved) {
 
 }
