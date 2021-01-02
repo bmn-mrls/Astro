c3335f45084da49eb6ce9c95a456283855d96b16
Benjamin Morales <bmnmrls@gmail.com>
2021-01-01 23:55:56 -0600

    💄 Update splash screen implementation

--- app/src/main/java/com/bmnmrls/astro/splash/views/SplashActivity.kt
+++ app/src/main/java/com/bmnmrls/astro/splash/views/SplashActivity.kt
@@ -2,42 +2,19 @@ package com.bmnmrls.astro.splash.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
-import com.bmnmrls.astro.databinding.ActivitySplashBinding
 import com.bmnmrls.astro.home.views.activity.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        setupViews()
+        launchHomeScreen()
     }
 
-    private fun setupViews() {
-        binding.splashMotionLayout.addTransitionListener(
-            object : MotionLayout.TransitionListener {
-                override fun onTransitionTrigger(
-                    p0: MotionLayout?,
-                    p1: Int,
-                    p2: Boolean,
-                    p3: Float
-                ) = Unit
-
-                override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
-                override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) =
-                    Unit
-
-                override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                    HomeActivity.launch(this@SplashActivity)
-                    finish()
-                }
-            }
-        )
+    private fun launchHomeScreen() {
+        HomeActivity.launch(this@SplashActivity)
+        finish()
     }
 
 }
