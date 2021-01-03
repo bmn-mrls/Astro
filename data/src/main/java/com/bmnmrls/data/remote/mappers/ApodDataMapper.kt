1c39e3b2f11a79c806e507fad7fbffecf5d4d794
Benjamin Morales <bmnmrls@gmail.com>
2021-01-03 00:21:12 -0600

    ✨ Add apod response and data mapper

---
+++ data/src/main/java/com/bmnmrls/data/remote/mappers/ApodDataMapper.kt
@@ -0,0 +1,20 @@
+package com.bmnmrls.data.remote.mappers
+
+import com.bmnmrls.data.remote.models.ApodResponse
+import com.bmnmrls.domain.mappers.Transformer
+import com.bmnmrls.domain.models.Apod
+
+class ApodDataMapper() : Transformer<ApodResponse, Apod>() {
+
+    override fun transform(value: ApodResponse): Apod = Apod(
+        value.copyright.orEmpty(),
+        value.date.orEmpty(),
+        value.explanation.orEmpty(),
+        value.hdUrl.orEmpty(),
+        value.mediaType.orEmpty(),
+        value.serviceVersion.orEmpty(),
+        value.title.orEmpty(),
+        value.url.orEmpty()
+    )
+
+}
---
+++ data/src/main/java/com/bmnmrls/data/remote/models/ApodResponse.kt
@@ -0,0 +1,22 @@
+package com.bmnmrls.data.remote.models
+
+import com.google.gson.annotations.SerializedName
+
+data class ApodResponse(
+    @SerializedName("copyright")
+    val copyright: String? = null,
+    @SerializedName("date")
+    val date: String? = null,
+    @SerializedName("explanation")
+    val explanation: String? = null,
+    @SerializedName("hdurl")
+    val hdUrl: String? = null,
+    @SerializedName("media_type")
+    val mediaType: String? = null,
+    @SerializedName("service_version")
+    val serviceVersion: String? = null,
+    @SerializedName("title")
+    val title: String? = null,
+    @SerializedName("url")
+    val url: String? = null
+)
