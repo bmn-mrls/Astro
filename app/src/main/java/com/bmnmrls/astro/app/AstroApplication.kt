3f0efd5c8a21b75aa0ee61ef7f96b84517f66d4c
Benjamin Morales <bmnmrls@gmail.com>
2021-01-02 20:35:37 -0600

    ✨ Add dependency injection and logger to the project

---
+++ app/src/main/java/com/bmnmrls/astro/app/AstroApplication.kt
@@ -0,0 +1,23 @@
+package com.bmnmrls.astro.app
+
+import android.app.Application
+import com.bmnmrls.astro.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class AstroApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupLogger()
+    }
+
+    private fun setupLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
