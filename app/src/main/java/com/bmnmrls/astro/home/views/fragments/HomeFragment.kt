c09b0600e59fb9ada9d921832a53d2bc7aa583f9
Benjamin Morales <bmnmrls@gmail.com>
2021-01-03 20:52:47 -0600

    ✨ Add base implementation for home fragment

--- app/src/main/java/com/bmnmrls/astro/home/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/bmnmrls/astro/home/views/fragments/HomeFragment.kt
@@ -1,8 +1,49 @@
 package com.bmnmrls.astro.home.views.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bmnmrls.astro.R
+import com.bmnmrls.astro.databinding.FragmentHomeBinding
+import com.bmnmrls.astro.home.adapters.ApodAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var apodAdapter: ApodAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Init the view model
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Init observers
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
