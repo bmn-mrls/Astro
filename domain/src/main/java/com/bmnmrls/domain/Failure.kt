f61d641d08c8a436dc329298a89830f37d5ac1f7
Benjamin Morales <bmnmrls@gmail.com>
2021-01-16 22:36:18 -0600

    ✨ Add data state and failure models definition

---
+++ domain/src/main/java/com/bmnmrls/domain/Failure.kt
@@ -0,0 +1,10 @@
+package com.bmnmrls.domain
+
+sealed class Failure {
+
+    class GenericError(val exception: Exception) : Failure()
+
+    object CustomError : Failure()
+
+    object NetworkConnection : Failure()
+}
